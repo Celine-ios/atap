c82f187cba4ec1359dfa4d00d8a1da7f5e26814c
wh0am123 <thejyjco@gmail.com>
2018-07-17 11:44:33 -0430

    Juan was here

--- assets/js/routes/index.js
+++ assets/js/routes/index.js
@@ -5,5 +5,13 @@ app.config(function($routeProvider) {
 		templateUrl: 'assets/templates/orders/index.html'
 	}).when('/reporte_movimientos', {
 		templateUrl: 'assets/templates/movements_report/index.html'
+	}).when('/agregar-gastos', {
+		templateUrl: 'assets/templates/spends/add/index.html'
+	}).when('/consultar-gastos', {
+		templateUrl: 'assets/templates/spends/query/index.html'
+	}).when('/editar-gastos', {
+		templateUrl: 'assets/templates/spends/edit/index.html'
+	}).when('/asignaciones', {
+		templateUrl: 'assets/templates/assignaments/index.html'
 	});
 });
