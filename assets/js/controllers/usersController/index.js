8c9a611dbc1bd93123584c20cc8adad76476ef51
wh0am123 <thejyjco@gmail.com>
2018-08-07 19:34:33 -0430

    Juan was here

--- assets/js/controllers/usersController/index.js
+++ assets/js/controllers/usersController/index.js
@@ -45,4 +45,22 @@ app.controller('usersControl', function($scope, $http){
 	    			console.log(response.data);
 	    	});
 	    };
+
+	    $scope.edit = id => {
+		var row = document.getElementsByTagName('tr')[id].innerHTML; // Selected Row from Table
+		var limit = id * 7; // Limited Key for columns array
+		var columns = []; // Columns Array
+		// begin variable: Indicates key begin
+		// Adding to columns array every column from begin to limit
+		for (var begin = (limit - 7); begin < limit; ++begin) {
+			    var column = document.getElementsByTagName('td')[begin].innerHTML;
+			    columns.push(column);
+		}
+		// Setting up a Item Session with every selected column
+		for (var i = 0; i < columns.length; ++i) {
+		    //sessionStorage.setItem('column-'+i, columns[i]);
+		    document.getElementsByName('column-'+i)[0].value = columns[i];
+		}
+
+	};
 });
